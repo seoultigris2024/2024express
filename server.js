c80e73e9668bf1bfbc9329868aa9d03b31163a1d
Daeyeol Kim <seoultigris@gmail.com>
2024-03-21 15:01:26 +0900

    re

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const users = [];
 
 app.use(express.json());
 
-app.get('/user', function (req, res) {
-  return res.send({users:users})
-})
+app.get("/user", function (req, res) {
+  return res.send({ users: users });
+});
 
-app.post("/user",function(req,res){
-    // console.log(req.body);
-    // users.push({name:"홍길동",age:30})
-    users.push({
-        name:req.body.name,
-        age:req.body.age
-    })
-    return res.send({success:true})
-})
+app.post("/user", function (req, res) {
+  // console.log(req.body);
+  // users.push({name:"홍길동3",age:30})
+  users.push({
+    name: req.body.name,
+    age: req.body.age,
+  });
+  return res.send({ success: true });
+});
 
-app.listen(3000)
+app.listen(3000);
